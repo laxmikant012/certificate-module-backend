79eabe20525f2036b7dc202e960697770a5f6ac6
lakshya <laxmikanty38@gmail.com>
2022-10-27 17:38:24 +0530

    flake8

--- repository/generate_certificate.py
+++ repository/generate_certificate.py
@@ -1,14 +1,11 @@
-from urllib import request
 from PIL import Image, ImageDraw, ImageFont
 import qrcode
 from .templates_config import coordinates_for_templates
 
 domain = "https://certificate-module-laxmikant.herokuapp.com"
 
+
 def generic_certificate_maker(**kwargs):
-
-    
-
     id = str(kwargs.get("id"))
     completion_date = str(kwargs.get("request").completion_date)
     issued_by = str(kwargs.get("request").issued_by).title()
@@ -19,29 +16,34 @@ def generic_certificate_maker(**kwargs):
     qr_code.add_data(url)
     qr_code.make()
     qr_image = qr_code.make_image()
-    template_id= kwargs.get("request").select_template
+    template_id = kwargs.get("request").select_template
     open_image = Image.open("templates/certificate"+str(template_id)+".png")
     draw_image = ImageDraw.Draw(open_image)
-    
-   
-
-    open_image.paste(qr_image, coordinates_for_templates[template_id]["qr_code"])
-    name_font = ImageFont.truetype('font/Amsterdam.ttf', coordinates_for_templates[template_id]["name_font"])
-    completion_date_font = ImageFont.truetype('font/Poppins-Medium.ttf', coordinates_for_templates[template_id]["completion_date_font"])
-    issued_by_font = ImageFont.truetype('font/Amsterdam.ttf', coordinates_for_templates[template_id]["issued_by_font"])
-    designation_font = ImageFont.truetype('font/Poppins-Medium.ttf', coordinates_for_templates[template_id]["designation_font"])     
-    draw_image.text(coordinates_for_templates[template_id]["name"], name, font=name_font, fill=(69, 69, 69), anchor='mm')
-    draw_image.text(coordinates_for_templates[template_id]["completion_date"], completion_date, font=completion_date_font, fill=(69, 69, 69), anchor='mm')
-    draw_image.text(coordinates_for_templates[template_id]["issued_by"], issued_by, font=issued_by_font, fill=(69, 69, 69), anchor='mm')
-    draw_image.text(coordinates_for_templates[template_id]["designation"], designation, font=designation_font, fill=(69, 69, 69), anchor='mm')
+    open_image.paste(qr_image,
+                     coordinates_for_templates[template_id]["qr_code"])
+    name_font = ImageFont.truetype('font/Amsterdam.ttf',
+                                   coordinates_for_templates
+                                   [template_id]["name_font"])
+    completion_date_font = ImageFont.truetype('font/Poppins-Medium.ttf',
+                                              coordinates_for_templates
+                                              [template_id]["completion_date_font"])
+    issued_by_font = ImageFont.truetype('font/Amsterdam.ttf',
+                                        coordinates_for_templates
+                                        [template_id]["issued_by_font"])
+    designation_font = ImageFont.truetype('font/Poppins-Medium.ttf',
+                                          coordinates_for_templates
+                                          [template_id]["designation_font"])
+    draw_image.text(coordinates_for_templates[template_id]["name"],
+                    name, font=name_font,
+                    fill=(69, 69, 69), anchor='mm')
+    draw_image.text(coordinates_for_templates[template_id]["completion_date"],
+                    completion_date, font=completion_date_font,
+                    fill=(69, 69, 69), anchor='mm')
+    draw_image.text(coordinates_for_templates[template_id]["issued_by"],
+                    issued_by, font=issued_by_font,
+                    fill=(69, 69, 69), anchor='mm')
+    draw_image.text(coordinates_for_templates[template_id]["designation"],
+                    designation, font=designation_font,
+                    fill=(69, 69, 69), anchor='mm')
     img_reduced_size = open_image.resize((1000, 720))
     img_reduced_size.save(f"certificates/{id}_certificate.png")
-
-
-
-
-
-
-
-    
-
--- routers/admin.py
+++ routers/admin.py
@@ -1,13 +1,11 @@
-from fastapi import APIRouter, status, UploadFile, File, Depends, Form, HTTPException
+from fastapi import APIRouter, status, HTTPException
+from fastapi import UploadFile, File, Depends, Form
 from database.database import get_db
 from fastapi.responses import FileResponse
 from repository import admin
 import schemas
 import datetime
-from .import oauth2
 from sqlalchemy.orm import Session
-
-
 
 router = APIRouter(
     tags=['admin'],
@@ -15,54 +13,65 @@ router = APIRouter(
 )
 
 
-
 @router.post('/user')
-def create_user(name : str = Form(), email : str = Form(), password : str = Form(), db : Session = Depends(get_db)):
-    request = {"name" : name, "email" : email, "password" : password}
+def create_user(name: str = Form(), email: str = Form(),
+                password: str = Form(), db: Session = Depends(get_db)):
+    request = {"name": name, "email": email, "password": password}
     request = schemas.User(**request)
     return admin.create_user(request, db)
 
 
 @router.post('/upload_csv', status_code=status.HTTP_201_CREATED)
-def upload_insert_csvfile_into_db(db : Session = Depends(get_db),  completion_date : datetime.date =  Form(), issued_by : str = Form(),
-                                  designation : str = Form(),   file: UploadFile = File(...), select_template : int = Form()):
-    request = {"completion_date" : completion_date, "issued_by" : issued_by, "designation" : designation, "select_template": select_template} 
+def upload_insert_csvfile_into_db(db: Session = Depends(get_db),
+                                  completion_date: datetime.date = Form(),
+                                  issued_by: str = Form(),
+                                  designation: str = Form(),
+                                  file: UploadFile = File(...),
+                                  select_template: int = Form()):
+    request = {"completion_date": completion_date, "issued_by": issued_by,
+               "designation": designation, "select_template": select_template}
     # validating and serializing uploaded user data
     request = schemas.UploadBase(**request)
     return admin.upload_insert_csvfile_into_db(db, request, file)
 
 
 @router.get('/show_all_certificates')
-def show_all_certificates(db : Session = Depends(get_db)):
+def show_all_certificates(db: Session = Depends(get_db)):
     return admin.show_all_certificates(db)
 
+
 @router.post('/find_certificate')
-def find_certificate(id : int = Form(), db : Session = Depends(get_db)):
+def find_certificate(id: int = Form(), db: Session = Depends(get_db)):
     return admin.find_certificate(id, db)
 
 
 @router.get('/verify_certificate/{id}')
-def verify_certificate(id , db : Session = Depends(get_db)):
+def verify_certificate(id, db: Session = Depends(get_db)):
     return admin.verify_certificate(id, db)
 
 
 @router.put('/update_details')
-def update_details(id : int = Form(), name  : str = Form(), db : Session = Depends(get_db)):
+def update_details(id: int = Form(), name: str = Form(),
+                   db: Session = Depends(get_db)):
     return admin.update_details(id, name, db)
 
 
 @router.delete('/delete_certificate')
-def delete_user(id : int = Form(), db  : Session = Depends(get_db)):
+def delete_user(id: int = Form(), db: Session = Depends(get_db)):
     return admin.delete_user(id, db)
+
 
 @router.get('/download_certificate')
 def download_certificate():
-    return FileResponse(admin.download_certificate(), media_type="application/pdf", filename='download.pdf')
+    return FileResponse(admin.download_certificate(),
+                        media_type="application/pdf", filename='download.pdf')
+
 
 @router.post('/download_single_certificate')
-def download_single_certificate(id : int = Form(), db : Session = Depends(get_db)):
+def download_single_certificate(id: int = Form(),
+                                db: Session = Depends(get_db)):
     if admin.download_single_certificate(id, db) == {'response': False}:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Certificate with {id} is not available")
-    return FileResponse(admin.download_single_certificate(id, db), media_type="application/pdf", filename="download.pdf")
-
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Certificate with {id} is not available")
+    return FileResponse(admin.download_single_certificate(id, db),
+                        media_type="application/pdf", filename="download.pdf")
